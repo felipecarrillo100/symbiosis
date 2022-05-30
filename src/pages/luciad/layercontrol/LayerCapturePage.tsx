3b8d2a267557d6b4a63308b567723cea42bf8933
felipecarrillo100 <felipecarrillo100@gmail.com>
2022-05-30 19:43:52 +0200

    useeffect for paramerters

--- src/pages/luciad/layercontrol/LayerCapturePage.tsx
+++ src/pages/luciad/layercontrol/LayerCapturePage.tsx
@@ -19,12 +19,13 @@ import {Map} from "@luciad/ria/view/Map";
 import {useHistory} from "react-router";
 import { useParams } from 'react-router';
 
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {AdvanceLayerTools} from "../../../components/luciad/layerutils/AdvanceLayerTools";
 import {TileManager} from "../../../utils/TileManager";
 import {DataBaseManager} from "../../../utils/DataBaseManager";
 import {DBSQLiteValues, SQLiteDBConnection} from "@capacitor-community/sqlite";
 import {ScreenMessage} from "../../../screen/ScreenMessage";
+import {LayerTreeNode} from "@luciad/ria/view/LayerTreeNode";
 
 interface StateProps {
     map: Map | null;
@@ -47,17 +48,69 @@ interface RaterTableEntry {
 const LayerCapturePage: React.FC = () => {
     const history = useHistory();
     const { layerId, x1, y1, x2, y2 } = useParams<{ layerId: string; x1: string; y1: string; x2: string; y2: string}>();
-
-    const pageTitle = "Capture tiles";
-
     const [inputs, setInputs] = useState({
         tableName: "",
         label: "",
         targetZoomLevels: 5,
     });
+    const pageTitle = "Capture tiles";
+    const [totalTiles, setTotalTiles] = useState(0);
+    const [minMax, setMinMax] = useState([] as number[]);
     const domainIndex = useRef(0);
-
     const tileManager = useRef(null as TileManager | null);
+    const [layer, setLayer] = useState(null as any);
+    const [samplingMode, setSamplingMode] = useState("");
+    const [dataType, setDataType] = useState("");
+    const [subdomains, setSubdomains] = useState([] as string[]);
+    const [levelCount, setLevelCount] = useState(20);
+    const [bounds, setBounds] = useState([] as number[]);
+    const [url, setUrl] = useState("");
+    const [layerName, setLayerName] = useState("");
+    const [calculatedZoomLevels, setCalculatedZoomLevels] = useState(0);
+
+    useEffect(()=>{
+        const cLayer = layerId && map ? AdvanceLayerTools.getLayerTreeNodeByID(map, layerId) as any : null;
+        setLayer(cLayer);
+        setTimeout(()=>{
+            const cInputs = {...inputs};
+
+            setCalculatedZoomLevels(inputs.targetZoomLevels);
+
+            if (cLayer && cLayer.model) {
+                if (cLayer.restoreCommand && cLayer.restoreCommand.parameters) {
+
+                    if (cLayer.restoreCommand.parameters.model.baseURL) {
+                        setUrl(cLayer.restoreCommand.parameters.model.baseURL);
+                        setSubdomains(cLayer.restoreCommand.parameters.model.subdomains as string[]);
+                    }
+                    if (cLayer.restoreCommand.parameters.model.levelCount) {
+                        setLevelCount(cLayer.restoreCommand.parameters.model.levelCount);
+                        tileManager.current = new TileManager({p1: {lon:Number(x1),lat:Number(y1)}, p2: {lon:Number(x2),lat:Number(y2)}}, levelCount);
+                        const result = tileManager.current.getTileRange(inputs.targetZoomLevels);
+                        setBounds(result.bounds);
+                    }
+                    if (cLayer.restoreCommand.parameters.model.url) {
+                        const layername = cLayer.restoreCommand.parameters.model.layer;
+                        const format = cLayer.restoreCommand.parameters.model.format;
+                        setUrl(cLayer.restoreCommand.parameters.model.url+`?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&LAYER=${layername}&STYLE=default&FORMAT=${format}&TILEMATRIXSET=GoogleMapsCompatible&TILEMATRIX={z}&TILEROW={-y}&TILECOL={x}`);
+                    }
+                    setDataType(cLayer.model.dataType);
+                    setSamplingMode(cLayer.model.samplingMode);
+                }
+                setLevelCount( cLayer.model.levelCount);
+                if (cLayer.label) {
+                    const tableName = pascalCase(cLayer.label);
+                    if (inputs.tableName !== tableName) {
+                        setInputs({...cInputs, tableName, label: tableName });
+                        setLayerName(tableName);
+                    }
+                }
+            }
+
+        })
+    }, [layerId, x1, y1, x2, y2 ])
+
+
 
     const { databaseManager, map } = useSelector<IAppState, StateProps>((state: IAppState) => {
         return {
@@ -67,10 +120,9 @@ const LayerCapturePage: React.FC = () => {
         }
     });
 
-
     const createTable = (db: SQLiteDBConnection | null, tableEntry: RaterTableEntry) =>{
         return new Promise<string | null>(resolve => {
-            const table = camelize(tableEntry.name);
+            const table = pascalCase(tableEntry.name);
             const tileSetsTableName = "rasters"
             if (db) {
                 const sqlDropIndexTable = `DROP TABLE IF EXISTS ${tileSetsTableName}`;
@@ -90,8 +142,8 @@ const LayerCapturePage: React.FC = () => {
         boundsY2 REAL 
     )
 `;
-                const sqlDropTable = `DROP TABLE IF EXISTS ${table};`;
-                const sqlCreateTable = `CREATE TABLE IF NOT EXISTS ${table}
+    const sqlDropTable = `DROP TABLE IF EXISTS ${table};`;
+    const sqlCreateTable = `CREATE TABLE IF NOT EXISTS ${table}
     (
          x INTEGER NOT NULL,
          y INTEGER NOT NULL,
@@ -174,7 +226,27 @@ const LayerCapturePage: React.FC = () => {
         }).catch(()=>{
 
         })
+    }
 
+    const onCalculate = () => {
+        if (layer && layer.model) {
+            const tableName = pascalCase(inputs.tableName);
+            if (inputs.tableName !== tableName) setInputs({...inputs, tableName})
+
+            tileManager.current = new TileManager({p1: {lon:Number(x1),lat:Number(y1)}, p2: {lon:Number(x2),lat:Number(y2)}}, levelCount);
+            const result = tileManager.current.getTileRange(inputs.targetZoomLevels);
+            setTotalTiles(result.totalTiles);
+            setCalculatedZoomLevels(result.maxLevel - result.minLevel);
+            setMinMax([result.minLevel, result.maxLevel]);
+
+            if (inputs.targetZoomLevels>6) {
+                ScreenMessage.warning("Too many tiles. Request less levels");
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     const onCancel = (event: any) => {
@@ -191,88 +263,49 @@ const LayerCapturePage: React.FC = () => {
         setInputs(newInputs);
     }
 
-    const layer = layerId && map ? AdvanceLayerTools.getLayerTreeNodeByID(map, layerId) as any: null;
-    const label = layer ? layer.label : "";
-    let url = "";
-    let subdomains = [] as string[];
-
-    let levelCount = 22;
-    let totalTiles = 0;
-    let bounds = [] as number[];
-    let calculatedZoomLevels = inputs.targetZoomLevels;
-    let minMax = [] as number [];
-    let dataType = "";
-    let samplingMode = "";
-    if (layer && layer.model) {
-        levelCount = layer.model.levelCount;
-        dataType = layer.model.dataType;
-        samplingMode = layer.model.samplingMode;
-
-        tileManager.current = new TileManager({p1: {lon:Number(x1),lat:Number(y1)}, p2: {lon:Number(x2),lat:Number(y2)}}, levelCount);
-        const result = tileManager.current.getTileRange(5);
-        totalTiles = result.totalTiles;
-        bounds = result.bounds;
-        calculatedZoomLevels = result.maxLevel - result.minLevel;
-        minMax = [result.minLevel, result.maxLevel];
-
-        if (layer.restoreCommand && layer.restoreCommand.parameters) {
-            if (layer.restoreCommand.parameters.layer.label) {
-                const tableName = camelize(layer.restoreCommand.parameters.layer.label);
-                if (inputs.tableName !== tableName) setInputs({...inputs, tableName})
-            }
-            if (layer.restoreCommand.parameters.model.baseURL) {
-                url = layer.restoreCommand.parameters.model.baseURL;
-                subdomains = layer.restoreCommand.parameters.model.subdomains as string[];
-            }
-            if (layer.restoreCommand.parameters.model.url) {
-                const layername = layer.restoreCommand.parameters.model.layer;
-                const format = layer.restoreCommand.parameters.model.format;
-                url = layer.restoreCommand.parameters.model.url+`?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&LAYER=${layername}&STYLE=default&FORMAT=${format}&TILEMATRIXSET=GoogleMapsCompatible&TILEMATRIX={z}&TILEROW={-y}&TILECOL={x}`;
-            }
-        }
-
-    }
 
     const onSubmit = (event: any) => {
         event.preventDefault();
         event.stopPropagation();
+        if (onCalculate()) {
+            setTimeout(()=>{
+                if (tileManager.current) {
+                    if (databaseManager && databaseManager.getDb()) {
+                        const db = databaseManager.getDb();
+                        const newTableEntry: RaterTableEntry = {
+                            name: inputs.tableName, title: inputs.label, description: "", size: totalTiles,
+                            minLevel: minMax[0], maxLevel:minMax[1],
+                            bounds,
+                            dataType,
+                            samplingMode
+                        };
+                        createTable(db, newTableEntry).then((realTableName)=>{
+                            if (realTableName) {
+                                let timer = 0
 
-        if (tileManager.current) {
-            if (databaseManager && databaseManager.getDb()) {
-                const db = databaseManager.getDb();
-                const newTableEntry: RaterTableEntry = {
-                    name: inputs.tableName, title: inputs.label, description: "", size: totalTiles,
-                    minLevel: minMax[0], maxLevel:minMax[1],
-                    bounds,
-                    dataType,
-                    samplingMode
-                };
-                createTable(db, newTableEntry).then((realTableName)=>{
-                    if (realTableName) {
-                        let timer = 0
-
-                        tileManager.current?.iterateTilesWithDelay(5, 10,(level: number,x: number,y: number) => {
-                            addTileToTable(realTableName, x,y,level);
-                            // console.log(`x: ${x} y: ${y} z:${level} `);
-                        }, () =>{
-                            ScreenMessage.info("Download completed");
-                        }, (counter, total) => {
-                            const ratio = counter/total * 100;
-                            const flag = Math.floor(total / 10);
-                            if (counter % flag === 0) {
-                                ScreenMessage.info("Percentage:" + ratio.toFixed(2) + "%" )
+                                tileManager.current?.iterateTilesWithDelay(5, 10,(level: number,x: number,y: number) => {
+                                    addTileToTable(realTableName, x,y,level);
+                                    // console.log(`x: ${x} y: ${y} z:${level} `);
+                                }, () =>{
+                                    ScreenMessage.info("Download completed");
+                                }, (counter, total) => {
+                                    const ratio = counter/total * 100;
+                                    const flag = Math.floor(total / 10);
+                                    if (counter % flag === 0) {
+                                        ScreenMessage.info("Percentage:" + ratio.toFixed(2) + "%" )
+                                    }
+                                });
                             }
                         });
+                    } else {
+                        ScreenMessage.warning("Connect to a database first");
                     }
-                });
-            } else {
-                ScreenMessage.warning("Connect to a database first");
-            }
-        } else {
-            ScreenMessage.warning("No tilemanager created")
+                } else {
+                    ScreenMessage.warning("No tilemanager created")
+                }
+                history.push("/page/Map");
+            })
         }
-
-        history.push("/page/Map");
     }
 
     return (
@@ -294,10 +327,9 @@ const LayerCapturePage: React.FC = () => {
                 </IonHeader>
                 {/*-- Content here --*/}
                 <form onSubmit={onSubmit}>
-
                     <IonItem>
                         <IonLabel position="floating">Layer name</IonLabel>
-                        <IonInput value={label} readonly/>
+                        <IonInput value={layerName} readonly/>
                     </IonItem>
                     <IonItem>
                         <IonLabel position="floating">URL:</IonLabel>
@@ -312,26 +344,25 @@ const LayerCapturePage: React.FC = () => {
                         <IonLabel position="floating">Levels available at server:</IonLabel>
                         <IonInput value={levelCount} readonly/>
                     </IonItem>
-                    <IonItem>
-                        <IonLabel position="floating">Requested zoom levels by client:</IonLabel>
-                        <IonInput value={inputs.targetZoomLevels} readonly/>
-                    </IonItem>
+
                     {/*  <IonItem>
                         <IonLabel position="floating">Calculated zoom levels:</IonLabel>
                         <IonInput value={calculatedZoomLevels} readonly/>
                     </IonItem> */}
-                    <IonItem>
-                        <IonLabel position="floating">Tile levels to retrieve:</IonLabel>
-                        <IonInput value={`[${minMax.join(", ")}]`} readonly/>
-                    </IonItem>
+
                     <IonItem>
                         <IonLabel position="floating">Subdomains available:</IonLabel>
                         <IonInput value={`[${subdomains.join(", ")}]`} readonly/>
                     </IonItem>
-                    <IonItem>
-                        <IonLabel position="floating">Estimated Total tiles</IonLabel>
-                        <IonInput value={totalTiles + " (approx. " + (totalTiles * 200 / 1024).toFixed(2) + " MBytes)"} readonly/>
+                    {/*<IonItem>
+                        <IonLabel position="floating">Datatype</IonLabel>
+                        <IonInput value={dataType} readonly/>
                     </IonItem>
+
+                    <IonItem>
+                        <IonLabel position="floating">SamplingMode</IonLabel>
+                        <IonInput value={samplingMode}  readonly/>
+                    </IonItem> */}
                     <IonItem>
                         <IonLabel position="floating">Table name</IonLabel>
                         <IonInput value={inputs.tableName} onIonChange={editInput} name="tableName"/>
@@ -340,16 +371,30 @@ const LayerCapturePage: React.FC = () => {
                         <IonLabel position="floating">Label</IonLabel>
                         <IonInput value={inputs.label} onIonChange={editInput} name="label"/>
                     </IonItem>
+                    <IonItem>
+                        <IonLabel position="floating">Requested zoom levels by client:</IonLabel>
+                        <IonInput value={inputs.targetZoomLevels} name="targetZoomLevels" onIonChange={editInput}/>
+                    </IonItem>
+                    <IonRow>
+                        <IonCol>
+                            <div className="ion-float-end">
+                                <IonButtons>
+                                    <IonButton type="button" onClick={onCalculate}>Calculate</IonButton>
+                                </IonButtons>
+                            </div>
+                        </IonCol>
+                    </IonRow>
 
                     <IonItem>
-                        <IonLabel position="floating">Datatype</IonLabel>
-                        <IonInput value={dataType} readonly/>
+                        <IonLabel position="floating">Tile levels to retrieve:</IonLabel>
+                        <IonInput value={`[${minMax.join(", ")}]`} readonly/>
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel position="floating">Estimated Total tiles</IonLabel>
+                        <IonInput value={totalTiles + " (approx. " + (totalTiles * 200 / 1024).toFixed(2) + " MBytes)"} readonly/>
                     </IonItem>
 
-                    <IonItem>
-                        <IonLabel position="floating">SamplingMode</IonLabel>
-                        <IonInput value={samplingMode}  readonly/>
-                    </IonItem>
+
                     <IonRow>
                         <IonCol>
                             <div className="ion-float-end">
@@ -366,10 +411,29 @@ const LayerCapturePage: React.FC = () => {
     );
 };
 
-function camelize(input: string) {
-    const str = input.replace(/[^a-zA-Z ]/g, "");
-    return str.replace(/(\w)(\w*)/g,
-        function(g0,g1,g2){return g1.toUpperCase() + g2.toLowerCase();}).replace(/\s/g, '');
+function pascalCase(str: string) {
+    const s = camelPad(str);
+    return s
+        .toLowerCase()
+        .replace(new RegExp(/[-_]+/, 'g'), ' ')
+        .replace(new RegExp(/[^\w\s]/, 'g'), '')
+        .replace(
+            new RegExp(/\s+(.)(\w*)/, 'g'),
+            ($1, $2, $3) => `${$2.toUpperCase() + $3}`
+        )
+        .replace(new RegExp(/\w/), s => s.toUpperCase());
+}
+
+function camelPad(str: string){ return str
+    // Look for long acronyms and filter out the last letter
+    .replace(/([A-Z]+)([A-Z][a-z])/g, ' $1 $2')
+    // Look for lower-case letters followed by upper-case letters
+    .replace(/([a-z\d])([A-Z])/g, '$1 $2')
+    // Look for lower-case letters followed by numbers
+    .replace(/([a-zA-Z])(\d)/g, '$1 $2')
+    .replace(/^./, function(str){ return str.toUpperCase(); })
+    // Remove any white space left around the word
+    .trim();
 }
 
 export {
